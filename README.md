72797ba6bf2d1b1db34630b7cf7ea71c0b0cc027
Bethan Hughes <bhughes@confluent.io>
2025-04-02 15:24:31 +0100

    init kstreams app

--- README.md
+++ README.md
@@ -1,4 +1,44 @@
-# gcp-qwiklabs-sql-automation
+# GCP Qwiklabs Sql Automation Kafka Streams App
+
+This repository provide a KStreams application for use with Google Qwiklabs Workshop: **AI-Powered SQL Automation & Real-Time Data Streaming with Confluent and Google Cloud.**
+
+The purpose of the application is to stream SQL queries from a Kafka topic, execute the queries on a BigQuery dataset, and then return the raw results to a Kafka topic.
+
+## Running the Application
+
+1. Clone the Github Repo
+
+```bash
+git clone https://github.com/confluentinc/gcp-qwiklabs-sql-automation.git
+```
+
+```bash
+cd gcp-qwiklabs-sql-automation
+```
+
+2. Set the following environment variables for Google and Confluent Cloud:
+
+```bash
+export GCP_PROJECT_ID="<YOUR_GCP_PROJECT_ID>"
+export KAFKA_API_KEY="<YOUR_KAFKA_API_KEY>"
+export KAFKA_API_SECRET="<YOUR_KAFKA_API_SECRET>"
+export BOOTSTRAP_SERVER="<YOUR_BOOTSTRAP_SERVER>"
+export SCHEMA_REGISTRY_URL="<YOUR_SCHEMA_REGISTRY_URL>"
+export SCHEMA_REGISTRY_KEY="<YOUR_SCHEMA_REGISTRY_API_KEY>"
+export SCHEMA_REGISTRY_SECRET="<YOUR_SCHEMA_REGISTRY_API_SECRET>"
+```
+
+3. Authenticate with gcloud
+
+```bash
+gcloud auth application-default login
+```
+
+3. Run the following command to build the maven jar
+
+```bash
+./mvnw clean install
+```
 
 This repository is part of the Confluent organization on GitHub.
 It is public and open to contributions from the community.
---
+++ src/main/java/io/confluent/kstreams/BigQueryClient.java
@@ -0,0 +1,105 @@
+package io.confluent.kstreams;
+
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.BigQueryError;
+import com.google.cloud.bigquery.BigQueryOptions;
+import com.google.cloud.bigquery.Job;
+import com.google.cloud.bigquery.JobId;
+import com.google.cloud.bigquery.JobInfo;
+import com.google.cloud.bigquery.QueryJobConfiguration;
+import com.google.cloud.bigquery.TableResult;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static io.confluent.kstreams.ExecuteQuery.DATASET;
+import static io.confluent.kstreams.ExecuteQuery.PROJECT_ID;
+import static io.confluent.kstreams.ExecuteQuery.display;
+
+@Slf4j
+public class BigQueryClient {
+
+    final BigQuery bigquery;
+    public BigQueryClient() {
+        // Initialize client that will be used to send requests.
+        bigquery = BigQueryOptions.getDefaultInstance().getService();
+    }
+
+    public String runQuery(String query) throws InterruptedException {
+        log.info(display, "Preparing to execute query in BigQuery");
+        // Sanitize query to ensure valid SQL syntax.
+        String cleanQuery = sanitizeQuery(query);
+        log.info(display, "Sanitized query to run: {}", cleanQuery);
+        // Create the query job configuration
+        QueryJobConfiguration queryConfig = QueryJobConfiguration.newBuilder(cleanQuery).setDefaultDataset(DATASET).build();
+        // Execute the query
+        Job queryJob = runQueryJob(queryConfig);
+        log.info(display, "Query execution completed");
+        // Get the results.
+        TableResult result = queryJob.getQueryResults();
+        log.info(display, "Getting results");
+        // Log the results for each row
+        result.iterateAll().forEach(values -> {
+            List<String> rowResults = new ArrayList<>();
+            values.forEach(value-> rowResults.add(value.getStringValue()));
+            log.info(display, "Result: {}", rowResults);
+        });
+
+        // Format the results.
+        return formatResults(result);
+    }
+
+    public String sanitizeQuery(String query) {
+        return query.replace("```sql", "").replace("```", "").trim();
+    }
+
+    public Job runQueryJob (QueryJobConfiguration queryConfig) {
+        // Create a unique job with job ID
+        UUID uuid = UUID.randomUUID();
+        String jobName = "qwiklabs_execute_sql_query_" + uuid;
+        JobId jobId = JobId.newBuilder().setProject(PROJECT_ID).setJob(jobName).build();
+        Job queryJob = bigquery.create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build());
+        log.info(display, "Built query job with ID: {}", jobId.toString());
+
+        // Wait for the query to complete.
+        try {
+            queryJob = queryJob.waitFor();
+            log.info(display, "Running query");
+        } catch (InterruptedException e){
+            log.error(display, "Query failed to complete due to error: {}", String.valueOf(e));
+        }
+
+        // Check for errors.
+        if (queryJob == null) {
+            throw new RuntimeException("Job no longer exists");
+        } else if (queryJob.getStatus().getExecutionErrors() != null && !queryJob.getStatus().getExecutionErrors().isEmpty()) {
+            List<String> errorMessages = getErrorMessage(queryJob);
+            throw new RuntimeException(String.valueOf(errorMessages));
+        }
+        return queryJob;
+    }
+
+    private static List<String> getErrorMessage(Job queryJob) {
+        List<String> errorMessages = new java.util.ArrayList<>();
+        List<BigQueryError> errors = queryJob.getStatus().getExecutionErrors();
+        for (BigQueryError e : errors) {
+            errorMessages.add(e.getMessage());
+        }
+        return errorMessages;
+    }
+
+    private static String formatResults(TableResult results) {
+        String formattedResults;
+        if (results.getTotalRows() == 1 ) {
+            formattedResults = results.getTotalRows() + " result. " + results;
+            log.info(display, "Returning {} row of results", results.getTotalRows());
+        } else {
+            formattedResults = results.getTotalRows() + " results. " + results;
+            log.info(display, "Returning {} rows of results", results.getTotalRows());
+        }
+        return formattedResults;
+    }
+
+}
